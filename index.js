451800b417dbb79c30a96b58de77487f467c81de
Parth099 <parthnpatel715@gmail.com>
2023-09-23 17:04:54 -0400

    added rekog endpoint

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const app = express();
 const port = +process.env.PORT;
 const allowedOrigins = ["http://localhost:3000"];
 
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+    // accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    // secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION, // Change to your preferred region
+});
+
+const rekognition = new AWS.Rekognition();
+
 // allow external requests
 const corsOptions = {
     origin: (origin, callback) => {
@@ -30,6 +40,43 @@ app.get("/health-check", async (req, res) => {
     console.log(`Example app listening on port ${port}`);
     res.status(200).json({
         status: "OK",
+    });
+});
+
+app.post("/recognize", (req, res) => {
+    const base64Image = req.body.image; // Get the base64 encoded image from the request body
+
+    if (!base64Image)
+        return res.status(400).json({
+            code: 400,
+            message: "error: failed to provide an image in request body",
+        });
+
+    const params = {
+        Image: {
+            Bytes: Buffer.from(base64Image, "base64"), // Convert base64 string to binary buffer
+        },
+        MinConfidence: 50,
+    };
+
+    rekognition.detectLabels(params, (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: "error: An error occurred with rekognition" });
+        }
+
+        const rekognitionResult = data.Labels;
+
+        const namesWithCounts = {};
+
+        rekognitionResult.forEach((label) => {
+            const { Name } = label;
+
+            if (!namesWithCounts[Name]) namesWithCounts[Name] = 0;
+            namesWithCounts[Name] += 1;
+        });
+
+        res.status(200).json(namesWithCounts);
     });
 });
 
