d2f5227d6323363a241f88eea0b8404979fc9a83
faiyazmislam <99035487+faiyazmislam@users.noreply.github.com>
2023-09-23 23:39:14 -0400

    added more comments

--- echo3dRoute.js
+++ echo3dRoute.js
@@ -1,94 +1,79 @@
 const express = require("express");
 const echo3DRouter = express.Router();
 
-//echo3d - faiyaz's api key
-const echo3DApiKey = "icy-heart-6014";
-
-////////1. gets user's garbage points from the database using their ID
 const AWS = require("aws-sdk");
 AWS.config.update({ region: "your-region" });
 
 const dynamodb = new AWS.DynamoDB();
-const userId =  "user-id"; 
+
+//echo3d - faiyaz's api key
+const echo3DApiKey = "icy-heart-6014";
+
+////////1. gets user's garbage points from the database using their ID
+const userID =  function getUserID(/*put outh code here to get user id*/ ){}
 
 //call db and get the points
-function getUserGarbagePoints(userId) {
+function getUserGarbagePoints(userID) {
   
     //update the code here to extract data from the table
     const params = {
     TableName: "YourTableName",
     Key: {
-      UserId: { S: userId },
+      UserID: { S: userID },
     },
   };
 
+  //get the user's points
   dynamodb.getItem(params, (err, data) => {
     if (err) {
-      console.error("Error fetching user points:", err);
-    } else {
-      const points = parseInt(data.Item.Points.N);
-      // Use the points to scale the 3D model in your Echo3D scene
-      // You'll need to implement Echo3D-specific logic here.
-
       
+        console.error("Error fetching user points:", err);
+    } else {
+      
+        //retrieve the data from the data table, parse it in any way //////////
+        const points = parseInt(data.Item.Points.N);
+        
+        //if points are retrieved, then render the object
+        renderEcho3Dobject(points);
+
     }
   });
 
 }
 
-//call the function
-const garbageSizeScale = getUserGarbagePoints(userId) *= .1;
+const axios = require('axios');
 
-//2. function that access camera on computer 
-const startCameraButton = document.getElementById("startCamera");
-const videoElement = document.getElementById("videoElement");
-
-// function to start the camera feed and initialize AR
-async function initializeAR() {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        videoElement.srcObject = stream;
-        await videoElement.play();
-
-        //call echo3D api here 
-        const echo3D = new Echo3D('icy-heart-6014'); 
-        const modelId = 'fb60a80e-68c7-4f61-a853-84b370448118'; 
-
-        const modelData = await echo3D.getModel(modelId);
-
-        const scene = new THREE.Scene();
-
-        const loader = new THREE.GLTFLoader();
-        const model = await new Promise((resolve, reject) => {
-            loader.load(modelData.url, resolve, undefined, reject);
-        });
-
-        scene.add(model.scene);
-
-        //sets the scale of the model with the user's garbage points
-        model.scene.scale.set(garbageSizeScale, garbageSizeScale, garbageSizeScale);
-
-        const renderer = new THREE.WebGLRenderer({ antialias: true });
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
-
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        camera.position.z = 5;
-
-        const animate = () => {
-            requestAnimationFrame(animate);
-            renderer.render(scene, camera);
-        };
-        animate();
-
-    } catch (error) {
-        console.error("Error accessing camera:", error);
-    }
+function renderEcho3DObject(userPoints) {
+    
+    // scale factor 
+    const scaleFactor = userPoints * 0.1;
+  
+    //echo3d - faiyaz's api key
+    const echo3dApiKey = 'icy-heart-6014';
+    const echo3dEndpoint = 'https://api.echo3d.co/v1/render';
+  
+    // request data parameters ////replace url of the model (trash bag)
+    const requestData = {
+      apiKey: echo3dApiKey,
+      model: '3D MODEL URL/////////////////////',
+      scale: scaleFactor,
+    };
+  
+    // api request for echo3d
+    axios.post(echo3dEndpoint, requestData)
+      .then((response) => {
+        
+        // render object is created here
+        const renderedObject = response.data.renderedObject;
+        console.log('Rendered 3D Object:', renderedObject);
+      })
+      .catch((error) => {
+        console.error('Error rendering 3D object:', error);
+      });
 }
-
-// Add a click event listener to the button to start AR
-startCameraButton.addEventListener("click", initializeAR);
-
+  
+// function to call points and render model
+retrieveUserPointsAndRender();
 
 echo3DRouter.get("/", function (req, res) {
     res.send("Get users controller");
