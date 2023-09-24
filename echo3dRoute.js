04a8698b5ce54ba25ff9eb23fb023d30b558cad8
faiyazmislam <99035487+faiyazmislam@users.noreply.github.com>
2023-09-23 23:19:34 -0400

    updated echo3d backend

--- echo3dRoute.js
+++ echo3dRoute.js
@@ -4,15 +4,18 @@ const echo3DRouter = express.Router();
 //echo3d - faiyaz's api key
 const echo3DApiKey = "icy-heart-6014";
 
-//gets user's garbage points from the database using their ID
+////////1. gets user's garbage points from the database using their ID
 const AWS = require("aws-sdk");
 AWS.config.update({ region: "your-region" });
 
 const dynamodb = new AWS.DynamoDB();
-const userId =  1; //"user-id"; // Replace with the user's actual ID
+const userId =  "user-id"; 
 
-function getUserPoints(userId) {
-  /*const params = {
+//call db and get the points
+function getUserGarbagePoints(userId) {
+  
+    //update the code here to extract data from the table
+    const params = {
     TableName: "YourTableName",
     Key: {
       UserId: { S: userId },
@@ -27,23 +30,16 @@ function getUserPoints(userId) {
       // Use the points to scale the 3D model in your Echo3D scene
       // You'll need to implement Echo3D-specific logic here.
 
+      
     }
-  });*/
+  });
 
-  if (userId == 1){
-    return 10;
-  }
-  else{
-    return 0;
-  }
 }
 
-getUserPoints(userId);
+//call the function
+const garbageSizeScale = getUserGarbagePoints(userId) *= .1;
 
-
-
-
-//function that access camera on computer 
+//2. function that access camera on computer 
 const startCameraButton = document.getElementById("startCamera");
 const videoElement = document.getElementById("videoElement");
 
@@ -54,8 +50,37 @@ async function initializeAR() {
         videoElement.srcObject = stream;
         await videoElement.play();
 
-        // You can now add your AR initialization code here
-        // This could include calling Echo3D and any other AR-related logic
+        //call echo3D api here 
+        const echo3D = new Echo3D('icy-heart-6014'); 
+        const modelId = 'fb60a80e-68c7-4f61-a853-84b370448118'; 
+
+        const modelData = await echo3D.getModel(modelId);
+
+        const scene = new THREE.Scene();
+
+        const loader = new THREE.GLTFLoader();
+        const model = await new Promise((resolve, reject) => {
+            loader.load(modelData.url, resolve, undefined, reject);
+        });
+
+        scene.add(model.scene);
+
+        //sets the scale of the model with the user's garbage points
+        model.scene.scale.set(garbageSizeScale, garbageSizeScale, garbageSizeScale);
+
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.z = 5;
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+        };
+        animate();
+
     } catch (error) {
         console.error("Error accessing camera:", error);
     }
@@ -63,9 +88,6 @@ async function initializeAR() {
 
 // Add a click event listener to the button to start AR
 startCameraButton.addEventListener("click", initializeAR);
-
-
-
 
 
 echo3DRouter.get("/", function (req, res) {
